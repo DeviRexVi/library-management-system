83409c5f7d7e76d75f6ac98d4cc8c00888167d4e
Davi Rex <rexdavi5855@gmail.com>
2026-07-14 17:28:10 -0300

    Implement showMenu method to App class

--- src/app/App.java
+++ src/app/App.java
@@ -1,19 +1,45 @@
 package src.app;
 
+import java.util.Scanner;
 import Library.Library;
 import Library.Book;
+
 public class App {
     private static App instance;
+    Library library = new Library();
+    Scanner scanner = new Scanner(System.in);
+    int opition;
 
-    private App(){}
-    public static App getInstance(){
-        if(instance == null){
+    private App() {
+    }
+
+    public static App getInstance() {
+        if (instance == null) {
             instance = new App();
         }
         return instance;
     }
 
-    public void run(){
-        
+    private void showMenu() {
+        System.out.println("""
+                ===============================
+                   Library Management System
+                ===============================
+
+                1. Add Book
+                2. Find Book
+                3. Borrow Book
+                4. Return Book
+                5. Remove Book
+                6. List Books
+                0. Exit
+                """);
+
+        System.out.print("Choose an option: ");
+    }
+
+    
+    public void run() {
+        showMenu();
     }
 }
